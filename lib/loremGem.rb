c0b3962bf88f2e573bb3c58de28e0d96ec4e0b81
Andrea Crisciotti <crisciotti.1689186@studenti.uniroma1.it>
2018-07-11 18:40:50 +0200

    abc

--- lib/loremGem.rb
+++ lib/loremGem.rb
@@ -3,72 +3,70 @@ require "loremGem/version"
 module LoremGem
 
   class << self
-# Inspired by Kaminari
-def load!
-  if rails?
-    register_rails_engine
-  elsif hanami?
-    register_hanami
-  elsif sprockets?
-    register_sprockets
-  end
+  # Inspired by Kaminari
+    def load!
+      if rails?
+        register_rails_engine
+        elsif hanami?
+          register_hanami
+          elsif sprockets?
+            register_sprockets
+          end
+          configure_sass
 
-  configure_sass
-end
+        end
+      end
+    end
 
-# Paths
-def gem_path
-  @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
-end
+    # Paths
+    def gem_path
+      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
+    end
 
-def stylesheets_path
-  File.join assets_path, 'stylesheets'
-end
+    def stylesheets_path
+      File.join assets_path, 'stylesheets'
+    end
 
-def javascripts_path
-  File.join assets_path, 'javascripts'
-end
+    def javascripts_path
+      File.join assets_path, 'javascripts'
+    end
 
-def assets_path
-  @assets_path ||= File.join gem_path, 'assets'
-end
+    def assets_path
+      @assets_path ||= File.join gem_path, 'assets'
+    end
 
-# Environment detection helpers
-def sprockets?
-  defined?(::Sprockets)
-end
+    # Environment detection helpers
+    def sprockets?
+      defined?(::Sprockets)
+    end
 
-def rails?
-  defined?(::Rails)
-end
+    def rails?
+      defined?(::Rails)
+    end
 
-def hanami?
-  defined?(::Hanami)
-end
+    def hanami?
+      defined?(::Hanami)
+    end
 
-private
+    private
 
-def configure_sass
-  require 'sass'
+    def configure_sass
+      require 'sass'
 
-  ::Sass.load_paths << stylesheets_path
-end
+      ::Sass.load_paths << stylesheets_path
+    end
 
-def register_rails_engine
-  require 'LoremGem/engine'
-end
+      def register_rails_engine
+        require 'LoremGem/engine'
+      end
 
-def register_sprockets
-  Sprockets.append_path(stylesheets_path)
-  Sprockets.append_path(javascripts_path)
-end
+      def register_sprockets
+        Sprockets.append_path(stylesheets_path)
+        Sprockets.append_path(javascripts_path)
+      end
 
-def register_hanami
-  Hanami::Assets.sources << assets_path
-end
-end
-end
-
-end
-
+      def register_hanami
+        Hanami::Assets.sources << assets_path
+      end
+    end
 LoremGem.load!
